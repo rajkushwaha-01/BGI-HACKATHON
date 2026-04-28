29587ff6224eb9bb92acaeee694e8a2e4396a542
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-04-28 14:14:59 +0530

    Add backend structure with server setup and database connection

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const app = require('./src/app')
+const connectDB = require('./src/config/db')
+
+connectDB();
+
+
+app.listen(3000 , ()=>{
+    console.log("PORT is running on 3000")
+});
